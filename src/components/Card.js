497958f5a09c9abd73835284b485a110397e9f43
omkar <omkar.desai916@gmail.com>
2021-11-14 01:56:42 +0530

    final

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 export default function (props) {
   return (
     <section>
-      <img className="travel-image" width="250" src={props.item.img} />
+      <img className="travel-image" width="240" src={props.item.img} />
       <div class="text-container">
         <h3>{props.item.Name}</h3>
         <span>{props.item.oneLine}</span>
